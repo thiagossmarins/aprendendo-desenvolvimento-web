8be3ab2cc28c3140a71aaaaff923f4c5460fe4ab
thiagossmarins <thiagodesouzasallesmarins@gmail.com>
2024-07-28 18:37:51 -0300

    aprendizado

---
+++ desenvolvimento-web/curso-php modulo-06/controle/switch.php
@@ -0,0 +1,46 @@
+<div class="titulo">Switch</div>
+
+<?php
+$categoria = 'Sedan';
+$preco = 0.0;
+$carro = ' ';
+
+if($categoria === 'luxo'){
+  $carro = "Mercedes";
+  $preco = 250000;
+} else if ($categoria === 'SUV') {
+  $carro = 'Renegade';
+  $preco = 80000;
+} else if ($categoria === 'Sedan') {
+  $carro = 'Etios';
+  $preco = 55000;
+} else {
+  $carro = 'Mobi';
+  $preco = 33000;
+}
+
+$precoFormatado = number_format($preco, 2, ',', '.');
+echo "<p>Carro; $carro<br> Preço: R$ $precoFormatado</p>";
+
+$categoria = '???';
+switch($categoria) {
+  case 'Luxo':
+    $carro = 'Mercedes';
+    $preco = 25000;
+    break;
+  case 'SUV';
+    $carro = 'Renegade';
+    $preco = 80000;
+    break;
+  case 'Sedan':
+    $carro = 'Etios';
+    $preco = 55000;
+    break;
+  default:
+  $carro = 'Mobi';
+  $preco = 33000;
+  break;
+}
+
+$precoFormatado = number_format($preco, 2, ',', '.');
+echo "<p>Carro; $carro<br> Preço: R$ $precoFormatado</p>";
--- desenvolvimento-web/curso-php modulo-06/index.php
+++ desenvolvimento-web/curso-php modulo-06/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
     <link rel="stylesheet" href="recursos/css/estilo.css">
     <title>Curso PHP</title>
 </head>
+
 <body>
     <header class="cabecalho">
         <h1>Curso PHP</h1>
@@ -157,6 +159,11 @@
                                 Operador ternário
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=controle&file=switch">
+                                Switch
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </nav>
@@ -166,4 +173,5 @@
         COD3R & ALUNOS © <?= date('Y'); ?>
     </footer>
 </body>
+
 </html>
