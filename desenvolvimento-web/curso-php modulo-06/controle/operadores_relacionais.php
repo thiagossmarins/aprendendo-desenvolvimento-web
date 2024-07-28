b39a0b746c049805781d9a5b864d02ce79671b86
thiagossmarins <thiagodesouzasallesmarins@gmail.com>
2024-07-28 16:46:58 -0300

    aprendizado

--- desenvolvimento-web/curso-php modulo-06/controle/operadores_relacionais.php
+++ desenvolvimento-web/curso-php modulo-06/controle/operadores_relacionais.php
@@ -11,17 +11,37 @@ var_dump(1 <> 1); //false
 
 var_dump(1 == "1"); //true
 var_dump(1 === "1"); //false
-echo "<br>"; 
-var_dump(1 != "1");//false
+echo "<br>";
+var_dump(1 != "1"); //false
 var_dump(1 !== "1"); //true
 
 echo "<p>Relacionais do if/else</p><br>";
 
 $idade = 18;
-if($idade < 18) {
+if ($idade < 18) {
   echo "Menor de idade<br>";
-} else if($idade <=65) {
+} else if ($idade <= 65) {
   echo  "Adulto";
 } else {
   echo "Terceira idade";
 }
+
+echo '<p>Spaceship</p><br>';
+# se o valor da esquerda for maior do que da direita, ele vai me retornar 1
+# se o valor for igual, ele vai me retornar zero
+# se o valor da direita for maior do que da esquerda, ele vai me retonar -1
+var_dump(5 <=> 3); 
+var_dump(50<=> 50);
+var_dump(5 <=> 50);
+
+?>
+
+<style>
+  p {
+    margin-bottom: -10px;
+  }
+
+  hr {
+    margin-bottom: 0px;
+  }
+</style>
